c36bcebbbfa53a2fe01ff5ea44ea577b31c68e69
ngavinsir <ngavinsir@gmail.com>
2020-07-09 23:48:29 +0700

    tailwind: add primary and neutral color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,36 @@
+const customColor = {
+  primary: {
+    1: "#E7FDFC",
+    2: "#B7FAF7",
+    3: "#87F7F1",
+    4: "#58F4EC",
+    5: "#28F1E6",
+    6: "#0ED7CD",
+    7: "#0BA79F",
+    8: "#087872",
+    9: "#054844"
+  },
+  neutral: {
+    1: "#D9D9D9",
+    2: "#BFBFBF",
+    3: "#A6A6A6",
+    4: "#8C8C8C",
+    5: "#737373",
+    6: "#595959",
+    7: "#404040",
+    8: "#262626",
+    9: "#0D0D0D"
+  }
+}
+
 module.exports = {
   purge: ["./src/**/*.svelte"],
   theme: {
-    extend: {},
+    extend: {
+      colors: customColor,
+      placeholderColor: customColor
+    },
   },
-  variants: {},
+  variants: ['responsive', 'hover', 'focus', 'group-hover', 'even', 'last', 'disabled'],
   plugins: [],
 }
