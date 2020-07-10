ecabf5e90578ad4a534c7be01c0c35ef27c7df4b
ngavinsir <ngavinsir@gmail.com>
2020-07-10 21:53:23 +0700

    products: responsive

--- src/routes/products/[slug].svelte
+++ src/routes/products/[slug].svelte
@@ -21,28 +21,31 @@
     export let data;
 </script>
 
-<div class="px-8 md:px-16 py-12 flex flex-col">
-   <span class="font-bold font-overpass text-neutral-2 text-xl">PRODUK LOKAL</span>
-   <span class="font-bold font-overpass text-primary-7 text-5xl mt-1">{data.name}</span>
-   <span class="font-bold font-overpass text-neutral-3 text-2xl mb-10">{data.category}</span>
+<div class="px-4 sm:px-8 md:px-16 py-12 flex flex-col">
+   <span class="font-bold font-overpass text-neutral-2 text-lg sm:text-xl">PRODUK LOKAL</span>
+   <span class="font-bold font-overpass text-primary-7 text-4xl sm:text-5xl mt-1">{data.name}</span>
+   <span class="font-bold font-overpass text-neutral-3 text-3xl sm:text-2xl mb-5 sm:mb-10">{data.category}</span>
 
    <Carousel perPage={{ 1100: 3, 800: 2 }} dots={false}>
       {#each ['images/budaya.jpg', 'images/landing-1.jpg', 'images/alam.jpg', 'images/kerajinan.jpg'] as photo, i}
-      <div class={i == 0 ? '' : 'pl-4'} style="height: 360px">
-         <img class="object-cover w-full h-full rounded-2xl" src={photo} alt={`foto-produk-${i}`} />
-      </div>
+         <div 
+            class="px-1 sm:px-4"
+            style="height: 360px"
+         >
+            <img class="object-cover w-full h-full rounded-2xl" src={photo} alt={`foto-produk-${i}`} />
+         </div>
       {/each}
    </Carousel>
 
-   <div class="flex flex-col lg:flex-row space-y-10 lg:space-y-0 lg:space-x-32 mt-20">
+   <div class="flex flex-col lg:flex-row space-y-6 lg:space-y-0 lg:space-x-32 mt-10 sm:mt-20">
       <div class="flex flex-col">
-         <p class="font-open-sans text-neutral-5 text-xl">{data.description}</p>
+         <p class="font-open-sans text-neutral-5 text-lg sm:text-xl">{data.description}</p>
          <Decorator />
       </div>
-      <div class="flex flex-col info-panel flex-shrink-0 self-start">
+      <div class="flex flex-col info-panel flex-shrink-0 self-start max-w-full">
          <div class="flex flex-col border-2 border-neutral-1 rounded-2xl px-6 py-3">
-            <span class="font-overpass font-bold text-neutral-3 text-lg">Harga</span>
-            <span class="font-overpass font-bold text-primary-7 text-2xl">{data.price}</span>
+            <span class="font-overpass font-bold text-neutral-3 text-base sm:text-lg">Harga</span>
+            <span class="font-overpass font-bold text-primary-7 text-xl sm:text-2xl">{data.price}</span>
          </div>
       </div>
    </div>
