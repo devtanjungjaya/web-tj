e48dc707f8d07fa624b7a48aeb5e7dda1e04d26c
ngavinsir <ngavinsir@gmail.com>
2020-07-10 15:02:21 +0700

    _layout: add playfair display font

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,11 +1,11 @@
 <svelte:head>
     <link rel="dns-prefetch" href="//fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,700;1,400&family=Overpass:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Overpass:wght@400;700&family=Playfair+Display:wght@700&display=swap" rel="stylesheet">
 </svelte:head>
 
 <Navbar />
-<div class="mt-20">
+<div class="my-20">
     <slot></slot>
 </div>
 
