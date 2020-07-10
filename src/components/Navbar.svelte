f5f42a59b710b8fb764c1664e0fde8f39df5f1de
ngavinsir <ngavinsir@gmail.com>
2020-07-10 15:02:05 +0700

    navbar: add z-index 10

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,6 +1,6 @@
 <div 
     class="flex items-center fixed top-0 w-full border-b-2 border-neutral-1 py-6 px-16 bg-white 
-    font-overpass font-bold text-neutral-3 text-xl space-x-20"
+    font-overpass font-bold text-neutral-3 text-xl space-x-20 z-10"
 >
     <a class="flex-grow">Home</a>
     <a>Kategori</a>
