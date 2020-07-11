808a7a82502545b6918672b7656602792092cdc2
ngavinsir <ngavinsir@gmail.com>
2020-07-12 02:58:56 +0700

    navbar: fix home href

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -2,7 +2,7 @@
     class="flex items-center fixed top-0 w-full border-b-2 border-neutral-1 py-5 px-16 bg-white 
     font-overpass font-bold text-neutral-3 text-xl space-x-20 z-10"
 >
-    <a rel="prefetch" href="index" class="mr-auto cursor-pointer hover:text-primary-7">Home</a>
+    <a rel="prefetch" href="/" class="mr-auto cursor-pointer hover:text-primary-7">Home</a>
     <a>Kategori</a>
     <a>Transportasi</a>
     <a>Peta</a>
