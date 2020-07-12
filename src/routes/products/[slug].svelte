2eb973295407656252e63fb9ddd8d190a996ea5d
ngavinsir <ngavinsir@gmail.com>
2020-07-12 23:33:05 +0700

    products/slug: max width left panel

--- src/routes/products/[slug].svelte
+++ src/routes/products/[slug].svelte
@@ -42,7 +42,7 @@
    </Carousel>
 
    <div class="flex flex-col lg:flex-row space-y-6 lg:space-y-0 lg:space-x-32 mt-10 sm:mt-20">
-      <div class="flex flex-col">
+      <div class="flex flex-col max-w-4xl">
          <p class="font-open-sans text-neutral-5 text-lg sm:text-xl">{data.description}</p>
          <Decorator />
          <Contact {...data.contact} />
