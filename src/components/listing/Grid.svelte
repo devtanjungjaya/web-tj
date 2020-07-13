c78c63fd644b80447709ac62122838361e077a4a
ngavinsir <ngavinsir@gmail.com>
2020-07-13 17:36:42 +0700

    listing: extract grid component

---
+++ src/components/listing/Grid.svelte
@@ -0,0 +1,18 @@
+<div class="grid gap-2 lg:gap-10 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 listing">
+    {#each items as item}
+        <div class="pb-2 md:pb-4 xl:pb-16 container-item">
+            <svelte:component this={itemComponent} {...item} />
+        </div>
+    {/each}
+</div>
+
+<script>
+    export let items;
+    export let itemComponent;
+</script>
+
+<style type="text/postcss">
+.listing {
+    justify-items: stretch;
+}
+</style>
