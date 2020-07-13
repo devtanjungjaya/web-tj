e5d7f6930c255d6c8851776d3d0e52b086aba9ce
ngavinsir <ngavinsir@gmail.com>
2020-07-13 18:16:17 +0700

    tours/listing: init

--- src/routes/tours/index.svelte
+++ src/routes/tours/index.svelte
@@ -1,23 +1,23 @@
 <script context="module">
     export function preload({ params, query }) {
-       return this.fetch(`products.json`)
+       return this.fetch(`tours.json`)
        .then(r => r.json())
-       .then(products => {
-          return { products };
+       .then(tours => {
+          return { tours };
        });
     }
 </script>
 
 <script>
-    import ProductItem from "../../components/listing/ProductItem.svelte";
+    import TourItem from "../../components/listing/TourItem.svelte";
     import Grid from "../../components/listing/Grid.svelte";
 
-    export let products;
+    export let tours;
 </script>
 
 <div class="flex flex-col px-2 md:px-6 lg:px-16 py-10">
     <Grid
-        items={products}
-        itemComponent={ProductItem}
+        items={tours}
+        itemComponent={TourItem}
     />
 </div>
