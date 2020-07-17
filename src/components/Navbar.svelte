a30bee9185303e330f869f873901bf69973e244e
ngavinsir <ngavinsir@gmail.com>
2020-07-17 18:04:46 +0700

    navbar: mobile menu

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,10 +1,21 @@
 <div 
-    class="flex items-center fixed top-0 w-full border-b-1 border-neutral-1 py-5 px-16 bg-white 
-    font-overpass font-bold text-neutral-3 text-xl space-x-20 z-20"
+    class="flex items-center fixed top-0 w-full border-b-1 border-neutral-1 py-5 px-4 md:px-6 lg:px-16 
+    bg-white font-overpass font-bold text-neutral-3 text-lg lg:text-xl z-20"
 >
     <a rel="prefetch" href="/" class="mr-auto cursor-pointer hover:text-primary-7">Home</a>
-    <a>Kategori</a>
-    <a>Transportasi</a>
-    <a>Peta</a>
-    <a>Kontak</a>
+    <div class="hidden sm:flex items-center space-x-10 lg:space-x-20">
+        <a>Kategori</a>
+        <a>Transportasi</a>
+        <a>Peta</a>
+        <a>Kontak</a>
+    </div>
+    <svg class="text-primary-7 sm:hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
+        <path 
+            fill-rule="evenodd" 
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 
+            01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" 
+            clip-rule="evenodd"
+        >
+        </path>
+    </svg>
 </div>
