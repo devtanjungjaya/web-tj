2b4174a24db49d760a045c15a1a7cbb00fc1b657
Felix <fogustino25@gmail.com>
2020-07-23 22:40:59 +0700

    size lg masih bermasalah

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -1,38 +1,38 @@
-<div class="flex flex-col font-overpass border-1 rounded-2xl max-w-lg m-4 sm:mx-8 sm:my-6 md:mx-16 md:my-12 lg:mx-16 lg:my-12 shadow p-5">    
-        <span class="font-bold text-neutral-5 text-xl md:text-3xl lg:text-3xl border-b-2">{name}</span>
+<div class="flex flex-col font-overpass border-1 rounded-2xl max-w-xl m-4 p-4 sm:p-6 sm:mx-8 sm:my-6 md:p-6 md:mx-16 md:my-12 lg:flex-none lg:p-6 lg:mx-16 lg:my-12 shadow ">
+        <span class="font-bold text-neutral-5 text-xl sm:text-3xl md:text-3xl lg:text-3xl border-b-2">{name}</span>
         <div class="flex flex-col mt-6 space-y-6">
             {#each phoneNumbers as phoneNumber, i}
-                <div class="flex items-start space-x-6">
+                <div class="flex items-start space-x-3 sm:space-x-6 md:space-x-6 lg:space-x-6">
                     <div class="flex flex-shrink-0 items-center justify-center rounded-full bg-primary-7 p-2 sm:p-3 md:p-3 lg:p-3">
                         <img class="w-4 h-4 sm:w-6 sm:h-6 md:w-6 md:h-6 lg:w-6 lg:h-6" src="ic_phone.svg" alt="phone" />
                     </div>
                     <div class="flex flex-col">
-                        <span class="font-bold text-neutral-3 text-sm md:text-lg lg:text-lg leading-none">
+                        <span class="font-bold text-neutral-3 text-xs sm:text-lg md:text-lg lg:text-lg leading-none">
                             {`No. HP/Whatsapp ${phoneNumbers.length > 1 ? i+1 : ''}`}
                         </span>
-                        <span class="font-semibold text-primary-7 text-lg hover:md:text-2xl lg:text-2xl">{phoneNumber}</span>
+                        <span class="font-semibold text-primary-7 text-sm sm:text-2xl hover:md:text-2xl lg:text-2xl">{phoneNumber}</span>
                     </div>
                 </div>
             {/each}
             {#if email}
-                <div class="flex items-start space-x-6">
+                <div class="flex items-start space-x-3 sm:space-x-6 md:space-x-6 lg:space-x-6">
                     <div class="flex flex-shrink-0 items-center justify-center rounded-full bg-primary-7 p-2 sm:p-3 md:p-3 lg:p-3">
                         <img class="w-4 h-4 sm:w-6 sm:h-6 md:w-6 md:h-6 lg:w-6 lg:h-6" src="ic_email.svg" alt="email" />
                     </div>
                     <div class="flex flex-col">
-                        <span class="font-bold text-neutral-3 text-sm md:text-lg lg:text-lg leading-none">Email</span>
-                        <span class="font-semibold text-primary-7 text-lg md:text-2xl lg:text-2xl">{email}</span>
+                        <span class="font-bold text-neutral-3 text-xs sm:text-lg md:text-lg lg:text-lg leading-none">Email</span>
+                        <span class="font-semibold text-primary-7 text-sm sm:text-2xl md:text-2xl lg:text-2xl">{email}</span>
                     </div>
                 </div>
             {/if}
             {#if address}
-                <div class="flex items-start space-x-6">
+                <div class="flex items-start space-x-3 sm:space-x-6 md:space-x-6 lg:space-x-6">
                     <div class="flex flex-shrink-0 items-center justify-center rounded-full bg-primary-7 p-2 sm:p-3 md:p-3 lg:p-3">
                         <img class="w-4 h-4 sm:w-6 sm:h-6 md:w-6 md:h-6 lg:w-6 lg:h-6" src="ic_address.svg" alt="address" />
                     </div>
                     <div class="flex flex-col">
-                        <span class="font-bold text-neutral-3 text-sm md:text-lg lg:text-lg leading-none">Alamat</span>
-                        <span class="font-semibold text-primary-7 text-lg md:text-2xl lg:text-2xl">{address}</span>
+                        <span class="font-bold text-neutral-3 text-xs sm:text-lg md:text-lg lg:text-lg leading-none">Alamat</span>
+                        <span class="font-semibold text-primary-7 text-sm sm:text-2xl md:text-2xl lg:text-2xl">{address}</span>
                     </div>
                 </div>
             {/if}
--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -3,10 +3,10 @@
 </svelte:head>
 
 <div class="flex flex-col font-overpass">
-    <h1 class="text-3xl sm:text-4xl md:text-5xl text-primary-7 font-bold px-4 sm:px-8 md:px-16 lg:px-16 pt-8">Kontak</h1>
+    <h1 class="text-3xl sm:text-5xl md:text-5xl text-primary-7 font-bold px-4 sm:px-8 md:px-16 lg:px-16 pt-8">Kontak</h1>
 
     <div class="grid lg:grid-cols-2">
-        <Contact name='Desa Wihbagus' phoneNumbers="0" email='felixogustino@mail.ugm.ac.id' address='jalan-jalan tbsilalaji kajsdkf'/>
+        <Contact name='Desa Wihbagus' phoneNumbers="0" email='felixogustino912@mail.ugm.ac.id' address='jalan-jalan tbsilalaji kajsdkf'/>
         <Contact name='Desa Wihbagus' phoneNumbers="0" email='coba@gmail.com' address='jalan-jalan'/>
         <Contact name='Desa Wihbagus' phoneNumbers="0" email='coba@gmail.com' address='jalan-jalan'/>
         <Contact name='Desa Wihbagus' phoneNumbers="0" email='coba@gmail.com' address='jalan-jalan'/>
