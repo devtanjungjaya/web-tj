dcd5b5a62485fad02d15842933eb812389e13714
prawiroht <prawiroht@gmail.com>
2020-07-28 10:06:32 +0700

    update index

--- src/routes/transport/route/index.svelte
+++ src/routes/transport/route/index.svelte
@@ -23,21 +23,22 @@
 </header>
 
 <section id="map">
-    <div class="map p-10">
-        <Map 
-        accessToken = {accToken} 
-        bind:this={map} 
-        on:recentre={e => console.log(e.detail.center.lat, e.detail.center.lng) } 
-        options = {{center,zoom:8}}
-        on:ready={addData}
-        >
-            <!-- <Marker lat={center.lat} lng={center.lng} color=#123123 label="My Location :)" popupClassName="class-name" /> -->
-            <NavigationControl />
-            <GeolocateControl options={{ some: 'control-option' }} />
-            <ScalingControl />
-        </Map>
+    <div class='p-10'>
+        <div class="map">
+            <Map 
+            accessToken = {accToken} 
+            bind:this={map} 
+            on:recentre={e => console.log(e.detail.center.lat, e.detail.center.lng) } 
+            options = {{center,zoom:8}}
+            on:ready={addData}
+            >
+                <Marker lat={-6.479478974609833} lng={105.65398972972787} color=#123123 label="TANJUNGJAYA" popupClassName="class-name" />
+                <NavigationControl />
+                <GeolocateControl options={{ some: 'control-option' }} />
+                <ScalingControl />
+            </Map>
+        </div>
     </div>
-    <nav id="filter-group" class="filter-group" bind:this={filterGroup}></nav>
 </section>
 
 
@@ -47,56 +48,9 @@
 .map {
     width: 100%;
     height: 30rem;
-    border-radius: 20px;
+    border-radius: 1rem;
 }
 
-.filter-group {
-    font: 12px/20px 'Helvetica Neue', Arial, Helvetica, sans-serif;
-    font-weight: 600;
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    z-index: 1;
-    border-radius: 3px;
-    width: 120px;
-    color: #fff;
-    }
-     
-    .filter-group input[type='checkbox']:first-child + label {
-    border-radius: 3px 3px 0 0;
-    }
-     
-    .filter-group label:last-child {
-    border-radius: 0 0 3px 3px;
-    border: none;
-    }
-     
-    .filter-group input[type='checkbox'] {
-    display: none;
-    }
-     
-    .filter-group input[type='checkbox'] + label {
-    background-color: #3386c0;
-    display: block;
-    cursor: pointer;
-    padding: 10px;
-    border-bottom: 1px solid rgba(0, 0, 0, 0.25);
-    }
-     
-    .filter-group input[type='checkbox'] + label {
-    background-color: #3386c0;
-    text-transform: capitalize;
-    }
-     
-    .filter-group input[type='checkbox'] + label:hover,
-    .filter-group input[type='checkbox']:checked + label {
-    background-color: #4ea0da;
-    }
-     
-    .filter-group input[type='checkbox']:checked + label:before {
-    content: '✔';
-    margin-right: 5px;
-}
 </style>
 
 <script>
@@ -130,241 +84,71 @@
             );
         });
     }
+
+    function addRoute(map, url,name){
+        map.addSource(name, {
+            'type': 'geojson',
+            'data': "images/" + url
+        });
+        map.addLayer({
+            'id': name,
+            'type': 'line',
+            'source': name,
+            'layout': {
+                'line-join': 'round',
+                'line-cap': 'round'
+            },
+            'paint': {
+                'line-color': '#123123',
+                'line-width': 3
+            }
+        });
+    }
+    function addPoint(map, url,name){
+        map.addSource(name, {
+            'type': 'geojson',
+            'data': "images/" + url
+        });
+        map.addLayer({
+            'id': name,
+            'type': 'circle',
+            'source': name,
+            'paint': {
+                'circle-radius': 4.5,
+                'circle-color': '#B42222'
+            }
+        });
+    }
+
     function addData(){
         console.log("test");
         tmp = map.getMap();
 
         // KRL
-        tmp.addSource('KRL', {
-            'type': 'geojson',
-            'data': "images/trans_krl.geojson"
-        });
-        tmp.addLayer({
-            'id': 'krl',
-            'type': 'line',
-            'source': 'KRL',
-            'layout': {
-                'line-join': 'round',
-                'line-cap': 'round'
-            },
-            'paint': {
-                'line-color': '#123123',
-                'line-width': 3
-            }
-        });
-        addFilter('krl')
+        addRoute(tmp,"trans_route_krl.geojson","KRL")
         // Angkot Mandala
-        tmp.addSource('Angkot_Mandala', {
-            'type': 'geojson',
-            'data': "images/trans_route_angkot_mandala.geojson"
-        });
-        tmp.addLayer({
-            'id': 'angkotmandala',
-            'type': 'line',
-            'source': 'Angkot_Mandala',
-            'layout': {
-                'line-join': 'round',
-                'line-cap': 'round'
-            },
-            'paint': {
-                'line-color': '#123123',
-                'line-width': 3
-            }
-        });
-
+        addRoute(tmp,"trans_route_angkot_mandala.geojson","Angkot_Mandala")
         // Angkot Rangkasbitung
-        tmp.addSource('Angkot_Rangkasbitung', {
-            'type': 'geojson',
-            'data': "images/trans_route_angkot_rangkasbitung.geojson"
-        });
-        tmp.addLayer({
-            'id': 'angkotrangkasbitung',
-            'type': 'line',
-            'source': 'Angkot_Rangkasbitung',
-            'layout': {
-                'line-join': 'round',
-                'line-cap': 'round'
-            },
-            'paint': {
-                'line-color': '#123123',
-                'line-width': 3
-            }
-        });
-
+        addRoute(tmp,"trans_route_angkot_rangkasbitung.geojson","Angkot_Rangkasbitung")
         // Bus AC
-        tmp.addSource('Bus_AC', {
-            'type': 'geojson',
-            'data': "images/trans_route_bus_ac.geojson"
-        });
-        tmp.addLayer({
-            'id': 'busac',
-            'type': 'line',
-            'source': 'Bus_AC',
-            'layout': {
-                'line-join': 'round',
-                'line-cap': 'round'
-            },
-            'paint': {
-                'line-color': '#123123',
-                'line-width': 3
-            }
-        });
-
+        addRoute(tmp,"trans_route_bus_ac.geojson","Bus_AC")
         // Bus Non AC
-        tmp.addSource('Bus_Non_AC', {
-            'type': 'geojson',
-            'data': "images/trans_route_bus_non_ac.geojson"
-        });
-        tmp.addLayer({
-            'id': 'busnonac',
-            'type': 'line',
-            'source': 'Bus_Non_AC',
-            'layout': {
-                'line-join': 'round',
-                'line-cap': 'round'
-            },
-            'paint': {
-                'line-color': '#123123',
-                'line-width': 3
-            }
-        });
-
+        addRoute(tmp,"trans_route_bus_non_ac.geojson","Bus_Non_AC")
         // Damri
-        tmp.addSource('Damri', {
-            'type': 'geojson',
-            'data': "images/trans_route_damri.geojson"
-        });
-        tmp.addLayer({
-            'id': 'damri',
-            'type': 'line',
-            'source': 'Damri',
-            'layout': {
-                'line-join': 'round',
-                'line-cap': 'round'
-            },
-            'paint': {
-                'line-color': '#123123',
-                'line-width': 3
-            }
-        });
-
+        addRoute(tmp,"trans_route_damri.geojson","Damri")
         // Damri AC
-        tmp.addSource('Damri_AC', {
-            'type': 'geojson',
-            'data': "images/trans_route_damri_ac.geojson"
-        });
-        tmp.addLayer({
-            'id': 'damriac',
-            'type': 'line',
-            'source': 'Damri_AC',
-            'layout': {
-                'line-join': 'round',
-                'line-cap': 'round'
-            },
-            'paint': {
-                'line-color': '#123123',
-                'line-width': 3
-            }
-        });
-
+        addRoute(tmp,"trans_route_damri_ac.geojson","Damri_AC")
         // ELF
-        tmp.addSource('ELF', {
-            'type': 'geojson',
-            'data': "images/trans_route_elf.geojson"
-        });
-        tmp.addLayer({
-            'id': 'elf',
-            'type': 'line',
-            'source': 'ELF',
-            'layout': {
-                'line-join': 'round',
-                'line-cap': 'round'
-            },
-            'paint': {
-                'line-color': '#123123',
-                'line-width': 3
-            }
-        });
-
+        addRoute(tmp,"trans_route_elf.geojson","ELF")
         // Ojek
-        tmp.addSource('Ojek', {
-            'type': 'geojson',
-            'data': "images/trans_route_ojek.geojson"
-        });
-        tmp.addLayer({
-            'id': 'ojek',
-            'type': 'line',
-            'source': 'Ojek',
-            'layout': {
-                'line-join': 'round',
-                'line-cap': 'round'
-            },
-            'paint': {
-                'line-color': '#123123',
-                'line-width': 3
-            }
-        });
-
+        addRoute(tmp,"trans_route_ojek.geojson","Ojek")
         // Bandara
-        tmp.addSource('Bandara', {
-            'type': 'geojson',
-            'data': "images/trans_point_bandara.geojson"
-        });
-        tmp.addLayer({
-            'id': 'bandara',
-            'type': 'circle',
-            'source': 'Bandara',
-            'paint': {
-                'circle-radius': 6,
-                'circle-color': '#B42222'
-            }
-        });
-
+        addPoint(tmp,"trans_point_bandara.geojson","Bandara")
         // Poin Penting
-        tmp.addSource('Poin_Penting', {
-            'type': 'geojson',
-            'data': "images/trans_point_poin_penting.geojson"
-        });
-        tmp.addLayer({
-            'id': 'poinpenting',
-            'type': 'circle',
-            'source': 'Poin_Penting',
-            'paint': {
-                'circle-radius': 6,
-                'circle-color': '#B42222'
-            }
-        });
-
+        addPoint(tmp,"trans_point_poin_penting.geojson","Poin_Penting")
         // Stasiun KRL
-        tmp.addSource('Stasiun_KRL', {
-            'type': 'geojson',
-            'data': "images/trans_point_stasiun_krl.geojson"
-        });
-        tmp.addLayer({
-            'id': 'stasiunkrl',
-            'type': 'circle',
-            'source': 'Stasiun_KRL',
-            'paint': {
-                'circle-radius': 6,
-                'circle-color': '#B42222'
-            }
-        });
-
+        addPoint(tmp,"trans_point_stasiun_krl.geojson","Stasiun_KRL")
         // Terminal Bis
-        tmp.addSource('Terminal_Bis', {
-            'type': 'geojson',
-            'data': "images/trans_point_terminal_bis.geojson"
-        });
-        tmp.addLayer({
-            'id': 'terminalbis',
-            'type': 'circle',
-            'source': 'Terminal_Bis',
-            'paint': {
-                'circle-radius': 6,
-                'circle-color': '#B42222'
-            }
-        });
-
-        
+        addPoint(tmp,"trans_point_terminal_bis.geojson","Terminal_Bis")
     }
 </script>
