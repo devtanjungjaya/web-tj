7ad727e679b9331553c1026dd6b1da518fd5f6e2
ngavinsir <ngavinsir@gmail.com>
2020-08-01 19:08:33 +0700

    item: add promo label

--- src/components/listing/ProductItem.svelte
+++ src/components/listing/ProductItem.svelte
@@ -1,8 +1,11 @@
 <a rel="prefetch" href={`products/${slug}`}>
     <div class="h-0 relative w-full" style="padding-bottom: 125%">
-        <div class="hover:shadow-md flex flex-col rounded-2xl border-1 border-neutral-1 overflow-hidden absolute w-full h-full">
+        <div class="hover:shadow-md flex flex-col rounded-2xl overflow-hidden absolute w-full h-full">
             <img class="product-img h-0 object-cover bg-neutral-1 min-w-0 min-h-0" src={photos[0].photoURI} alt={name}/>
-            <div class="p-2 xs:p-3 flex-grow flex flex-col">
+            <div 
+                class="p-2 xs:p-3 flex-grow flex flex-col border-1 border-t-0 border-neutral-1
+                rounded-2xl rounded-t-none"
+            >
                 <span 
                     class="font-bold font-overpass text-primary-7 leading-none xs:leading-tight 
                     text-base xs:text-lg max-line-2"
@@ -11,15 +14,24 @@
                 </span>
                 <span 
                     class="font-overpass font-normal text-neutral-3 flex-grow 
-                    text-xs xs:text-base max-line-1"
+                    text-sm xs:text-base max-line-1"
                 >
                     {contact.name}
                 </span>
-                <span class="self-end font-overpass font-bold text-neutral-9 text-xs xs:text-base">
+                <span class="self-end font-overpass font-bold text-neutral-9 text-sm xs:text-base">
                     {price}
                 </span>
             </div>
         </div>
+        {#if promotions.length}
+        <span 
+            class="bg-primary-7 py-2 xs:py-3 px-1 xs:px-2 top-0 absolute font-overpass font-bold text-white 
+            rounded-b-lg text-sm xs:text-base shadow-lg" 
+            style="right: 1.5rem"
+        >
+            Promo
+        </span>
+        {/if}
     </div>
 </a>
 
@@ -37,6 +49,7 @@
     export let contact;
     export let prices;
     export let slug;
+    export let promotions;
 
     $: price = formatRupiah(prices[0].value);
 </script>
