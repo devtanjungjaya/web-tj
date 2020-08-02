35799c1646fbb95fa3021edb2047b731c2589f1a
ngavinsir <ngavinsir@gmail.com>
2020-08-02 18:49:17 +0700

    listing: init seo

--- src/routes/products/index.svelte
+++ src/routes/products/index.svelte
@@ -15,6 +15,9 @@
     import PriceFilter from "../../components/filter/PriceRange.svelte";
     import Navigation from "../../components/listing/Navigation.svelte";
 
+    const title = "Cari Produk Lokal di Buffer Zone KEK Tanjung Lesung";
+    const description = "Cari Produk Lokal di Buffer Zone KEK Tanjung Lesung";
+
     export let products = [];
     export let productCategories;
     export let promotions;
@@ -57,6 +60,18 @@
     ];
 
 </script>
+
+<svelte:head>
+   <title>{title}</title>
+   <meta name="description" content={description} />
+   <meta property="og:title" content={title} />
+   <meta property="og:type" content="website" />
+   <meta property="og:description" content={description} />
+   <meta property="og:image" content="images/landing-1.webp" />
+   <meta name="twitter:title" content={title}>
+   <meta name="twitter:description" content={description}>
+   <meta name="twitter:image" content="images/landing-1.webp">
+</svelte:head>
 
 <div class="flex flex-col px-2 xs:px-4 md:px-6 lg:px-16 py-6 lg:py-10 min-h-screen">
     <Grid
