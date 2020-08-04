bf927ede9401d1a15e75b37e7059b26cddf42e77
ngavinsir <ngavinsir@gmail.com>
2020-08-04 18:47:09 +0700

    item: use custom lazy load img in photos and photoDetail

--- src/components/Image.svelte
+++ src/components/Image.svelte
@@ -9,6 +9,7 @@
     export let alt;
 
     let loaded = false;
+    let error = false;
 
     function lazy(node, data) {
 		if (imgCache.has(data.src)) {
@@ -24,10 +25,7 @@
             };
             img.onerror = () => {
                 node.setAttribute('src', 'ic_no_photo.svg');
-                node.classList.remove('w-full');
-                node.classList.remove('h-full');
-                node.classList.add("h-10");
-                node.classList.add("w-10");
+                error = true;
                 loaded = true;
             }
 		}
@@ -39,11 +37,11 @@
 </script>
 
 <div 
-    class={`w-full h-full flex items-center justify-center ${loaded ? 'bg-neutral-1' : 'skeleton-box'} 
-    ${wrapperClass}`}
+    class={`w-full h-full flex items-center justify-center ${loaded ? '' : 'skeleton-box'} 
+    ${wrapperClass} ${error ? 'bg-neutral-1' : ''}`}
 >
     <img
-        class={`w-full h-full ${imgClass}`}
+        class={`${error ? 'h-10 w-10' : 'w-full h-full'} ${imgClass}`}
         class:invisible={!loaded}
         {alt}
         use:lazy={{ src }}
