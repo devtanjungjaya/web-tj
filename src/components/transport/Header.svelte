169121c748ef72eea38502a4028f674c39ce523b
ngavinsir <ngavinsir@gmail.com>
2020-08-05 00:44:22 +0700

    transport: adjust some styling

--- src/components/transport/Header.svelte
+++ src/components/transport/Header.svelte
@@ -1,12 +1,12 @@
 <script>
     export let url;
 </script>
-<div id="head" class="py-4 px-4 sm:px-8 md:px-16">
-    <h1 class="text-3xl md:text-4xl xl:text-5xl text-primary-7 font-bold">Transportasi</h1>
+<div id="head" class="pt-5 px-4 sm:px-8 md:px-16">
+    <h1 class="text-3xl md:text-4xl xl:text-5xl text-primary-7 font-bold font-overpass">Transportasi</h1>
 </div>
 
-<div id="route" class="flex flex-row px-4 sm:px-8 md:px-16 font-bold text-sm sm:text-base md:text-lg">
-    <div class="py-4 pr-4">
+<div id="route" class="flex flex-row px-4 sm:px-8 md:px-16 font-bold text-sm sm:text-base md:text-lg font-overpass">
+    <div class="py-2 pr-4">
         <a href="/transport">
             {#if url == "map"}
                 <h2 class="text-primary-7">Peta</h2>
@@ -16,7 +16,7 @@
         </a>
     </div>
     <br>
-    <div class="py-4 px-4">
+    <div class="py-2 px-4">
         <a href="/transport/route">
             {#if url == "map"}
                 <h2 class="text-neutral-2">Jalur</h2>
