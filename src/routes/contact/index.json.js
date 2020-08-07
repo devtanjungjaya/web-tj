ad0a434c9081533ce7b6932d81a8879169ab34e3
Felix Ogustino <fogustino25@gmail.com>
2020-08-07 15:52:27 +0700

    fix contact

--- src/routes/contact/index.json.js
+++ src/routes/contact/index.json.js
@@ -1,10 +1,7 @@
 import { adminContactsMap } from '../../utilities/contact';
 
-const contents = JSON.stringify([...adminContactsMap.values()])
-
-console.log(adminContactsMap)
-console.log(adminContactsMap.values())
-console.log(contents)
+const contents = JSON.stringify(
+	[...adminContactsMap.values()])
 
 export function get(req, res) {
 	res.writeHead(200, {
--- src/routes/maps/mitigasi-bencana/index.svelte
+++ src/routes/maps/mitigasi-bencana/index.svelte
@@ -12,7 +12,7 @@
     import Header from '../../../components/maps/Header.svelte'
     
     const { GeolocateControl, NavigationControl, ScalingControl } = controls
-    const mapboxAccessToken = "MAPBOX_ACCESS_TOKEN";
+    const mapboxAccessToken = "pk.eyJ1IjoiZmVsaXgyNSIsImEiOiJja2Q0MWZrMmcwMjBkMnpzODFjYW1vbG83In0.IKpXqrohe3JR1bKq_3JSDA";
     let map;
     let center = { lat: -6.493857222350371, lng: 105.7747242749806}; 
     let colors = [
