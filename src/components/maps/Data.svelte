bc6556d7b5ab77b529755cd5f65cdfc37ff9ad85
prawiroht <prawiroht@gmail.com>
2020-08-07 15:58:14 +0700

    update peta felix

---
+++ src/components/maps/Data.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let tmp;
+    let map = tmp.getMap();
+    let data = [
+        {
+            name:"",
+            url:"",
+            color:"",
+        },
+    ]
+
+    data.forEach(element => {
+        map.addSource(element.name,{
+            type:"geojson",
+            data: element.url
+        })
+        map.addLayer({
+
+        })
+    });
+</script>
--- src/routes/maps/fasilitas-kesehatan/index.svelte
+++ src/routes/maps/fasilitas-kesehatan/index.svelte
@@ -1,13 +1,14 @@
 <Header url="fasilitas-kesehatan"/>
 <section>
-    <div class="h-64 sm:h-96 md:h-128 mapbox py-4 px-4 sm:px-8 md:px-16 flex flex-row rounded-2xl">
-        <div class="map border-1 border-neutral-1 w-3/4 h-96 overflow-hidden rounded-l-2xl">
+    <div class="h-64 sm:h-96 md:h-128 mapbox py-4 px-4 sm:px-8 md:px-16 flex flex-row">
+        <div class="map border-1 border-neutral-1 w-full overflow-hidden rounded-2xl">
             <Map
                 accessToken = {mapboxAccessToken}
                 bind:this = {map}
                 options = {{center,zoom:9}}
             >
-            <NavigationControl />
+            <NavigationControl position="top-left"/>
+            <Legends map={map.getMap()} layers={map.getMap().getStyle().layers} colors={colors} check={check} type={type} typeURL={typeURL}/>
             </Map>
         </div>
     </div>
@@ -15,22 +16,25 @@
 
 
 <style type="text/postcss">
-    .map{
-        width: 75%;
-        height: 100%;
-    }
+
 </style>
 
 <script id="map">
     import { Map, Geocoder, Marker, controls } from '@beyonk/svelte-mapbox'
     import Header from '../../../components/maps/Header.svelte'
+    import Legends from '../../../components/maps/Legends.svelte'
+
     const { GeolocateControl, NavigationControl, ScalingControl } = controls
     const mapboxAccessToken = "MAPBOX_ACCESS_TOKEN";
     let map;
     let center = { lat: -6.5116, lng: 105.6530};
-
+    let colors = []
+    let check = []
+    let type = []
+    let typeURL = []
     const title = "Peta-Peta Khusus Buffer Zone KEK Tanjung Lesung"
     const description = "Peta-Peta Khusus Buffer Zone KEK Tanjung Lesung"
+    
 </script>
 
 <svelte:head>
