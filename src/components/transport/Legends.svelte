6dfab66df548ad439ed99dcb01cdda728c8b99eb
prawiroht <prawiroht@gmail.com>
2020-08-08 18:17:30 +0700

    optimize code

--- src/components/transport/Legends.svelte
+++ src/components/transport/Legends.svelte
@@ -1,10 +1,9 @@
 <script>
-    export let map;
-    export let layers = [];
-    export let colors = [];
-    export let check = [];
-    export let typeURL = [];
-    export let type = [];
+    
+    import { dataTransportasi, dataLokasi } from "./Data.svelte"
+    export let tmp;
+    let map = tmp.getMap()
+    let data = dataTransportasi.concat(dataLokasi);
     $: innerWidth = 0;
     function changeFunc(id,checked){
         map.setLayoutProperty(
@@ -50,38 +49,38 @@
 
 <svelte:window bind:innerWidth></svelte:window>
 
-<div id="filter-group" class="filter-group flex flex-col relative border-1 border-neutral-1">
+<div id="filter-group" class="float-right filter-group flex flex-col relative bg-white">
     <div class="legenda border-1 border-neutral-2 px-2 sm:px-4 align-middle text-primary-7 text-xs sm:text-base md:text-lg font-bold relative">
         <h1 class="relative">Legenda</h1>
     </div>
     <div class="filter flex flex-col text-primary-6 p-0">
-        {#each colors as color,i }
-            {#if check[i]}
+        {#each data as element }
+            {#if element.check}
                 {#if innerWidth > 640}
                     <label 
                         class='box flex flex-row py-2 px-2 hover:text-primary-7 hover:bg-gray-100 cursor-pointer text-neutral-5 
                         font-bold border-1 border-neutral-1 items-center last:border-0 sm:text-xs md:text-sm'
                     >
-                        {#if (type[i] != "point")}
+                        {#if (element.type != "point")}
                             <div class="h-5 w-5">
-                                <div class="relative checkbox align-middle mx-1 rounded-full" style="--theme-color: {color}; background-color: var(--theme-color);"></div>
+                                <div class="relative checkbox align-middle mx-1 rounded-full" style="--theme-color: {element.color}; background-color: var(--theme-color);"></div>
                             </div>
                         {:else}
                             <div class="h-5 w-5">
-                                <img src="{typeURL[i]}.png" alt="ENA-ENA ( ͡° ͜ʖ ͡°)" class="w-full h-full mx-1">
+                                <img src="{element.url}.png" alt="ENA-ENA ( ͡° ͜ʖ ͡°)" class="w-full h-full mx-1">
                             </div>
                         {/if}
-                        <input type="checkbox" class= "opacity-0" on:change={changeFunc(layers[111+i].id,check[i])} bind:checked={check[i]}>
-                        <p class="float-right overflow-hidden">{layers[111+i].id}</p>
+                        <input type="checkbox" class= "opacity-0" on:change={changeFunc(element.name,element.check)} bind:checked={element.check}>
+                        <p class="float-right overflow-hidden">{element.name}</p>
                     </label>
                 {:else}
                     <label 
                         class='box flex flex-row py-2 px-2 hover:text-primary-7 hover:bg-gray-100 cursor-pointer align-middle 
                         text-neutral-5 font-bold border-2 items-center'
-                        style="--theme-color: {color}; border-color: var(--theme-color);"
+                        style="--theme-color: {element.color}; border-color: var(--theme-color);"
                     >
-                        <p class="overflow-hidden">{layers[111+i].id}</p>
-                        <input type="checkbox" class= "opacity-0" on:change={changeFunc(layers[111+i].id,check[i])} bind:checked={check[i]}>
+                        <p class="overflow-hidden">{element.name}</p>
+                        <input type="checkbox" class= "opacity-0" on:change={changeFunc(element.name,element.check)} bind:checked={element.check}>
                     </label>
                 {/if}
             {:else}
@@ -90,13 +89,13 @@
                         <div class="h-5 w-5">
                             <div class="checkbox align-middle mx-1 bg-neutral-1 text-neutral-1 rounded-full"></div>
                         </div>
-                        <input id={layers[111+i].id} type="checkbox" class= "opacity-0"  on:change={changeFunc(layers[111+i].id,check[i])} bind:checked={check[i]}>
-                        <p class="overflow-hidden">{layers[111+i].id}</p>
+                        <input id={element.name} type="checkbox" class= "opacity-0"  on:change={changeFunc(element.name,element.check)} bind:checked={element.check}>
+                        <p class="overflow-hidden">{element.name}</p>
                     </label>
                 {:else}
                     <label class='box flex flex-row py-2 px-2 hover:text-primary-7 hover:bg-gray-100 cursor-pointer text-neutral-5 border-1 border-neutral-1 italic items-center'>
-                        <p class="w-full overflow-hidden">{layers[111+i].id}</p>
-                        <input id={layers[111+i].id} type="checkbox" class= "opacity-0"  on:change={changeFunc(layers[111+i].id,check[i])} bind:checked={check[i]}>
+                        <p class="w-full overflow-hidden">{element.name}</p>
+                        <input id={element.name} type="checkbox" class= "opacity-0"  on:change={changeFunc(element.name,element.check)} bind:checked={element.check}>
                     </label>
                 {/if}
                 
