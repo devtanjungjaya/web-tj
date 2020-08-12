8be6b86333adadf31b761796e036e8037a23497d
ngavinsir <ngavinsir@gmail.com>
2020-08-12 21:19:58 +0700

    sitemap: fix url

--- src/routes/sitemap.xml.js
+++ src/routes/sitemap.xml.js
@@ -8,32 +8,32 @@ const { Readable } = require( 'stream' )
 export async function get(req, res, next) {
     const siteUrls = [
         { url: "/", changefreq: "monthly", priority: 0.5 },
-        { url: "/penginapan/", changefreq: "weekly", priority: 0.8 },
-        { url: "/produk-lokal/", changefreq: "weekly", priority: 0.8 },
-        { url: "/destinasi-wisata/", changefreq: "weekly", priority: 0.8 },
-        { url: "/paket-wisata/", changefreq: "weekly", priority: 0.8 }
+        { url: "/penginapan", changefreq: "weekly", priority: 0.8 },
+        { url: "/produk-lokal", changefreq: "weekly", priority: 0.8 },
+        { url: "/destinasi-wisata", changefreq: "weekly", priority: 0.8 },
+        { url: "/paket-wisata", changefreq: "weekly", priority: 0.8 }
     ];
 
     const roomUrls = [...rooms.keys()].map(slug => ({
-        url: `/penginapan/${slug}/`,
+        url: `/penginapan/${slug}`,
         changefreq: "monthly",
         priority: 1.0
     }));
 
     const productUrls = [...products.keys()].map(slug => ({
-        url: `/produk-lokal/${slug}/`,
+        url: `/produk-lokal/${slug}`,
         changefreq: "monthly",
         priority: 1.0
     }));
 
     const destinationUrls = [...destinations.keys()].map(slug => ({
-        url: `/destinasi-wisata/${slug}/`,
+        url: `/destinasi-wisata/${slug}`,
         changefreq: "monthly",
         priority: 1.0
     }));
 
     const tourUrls = [...tours.keys()].map(slug => ({
-        url: `/paket-wisata/${slug}/`,
+        url: `/paket-wisata/${slug}`,
         changefreq: "monthly",
         priority: 1.0
     }));
