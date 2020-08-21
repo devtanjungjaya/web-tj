b9d4487fb407ee3b2460b7221424e32a74da7c43
ngavinsir <ngavinsir@gmail.com>
2020-08-22 03:02:24 +0700

    legends: fix styling

--- src/components/maps/Legends.svelte
+++ src/components/maps/Legends.svelte
@@ -57,22 +57,23 @@
                         font-bold border-1 border-neutral-1 items-center last:border-0 sm:text-xs md:text-sm'
                     >
                         {#if (X.type != "point")}
-                            <div class="h-5 w-5">
-                                <div class="relative checkbox align-middle mx-1 rounded-full" style="--theme-color: {X.color}; background-color: var(--theme-color);"></div>
-                            </div>
+                            <div 
+                                class="h-5 w-5 flex-shrink-0 mr-2 rounded-full" 
+                                style={`background-color: ${X.color}`}
+                            />
                         {:else}
-                            <div class="h-5 w-5">
-                                <img src="{X.url}.png" alt="ENA-ENA ( ͡° ͜ʖ ͡°)" class="w-full h-full mx-1">
+                            <div class="h-5 w-5 mr-2 flex-shrink-0">
+                                <img src="{X.url}.png" alt="icon-legend" class="w-full h-full">
                             </div>
                         {/if}
-                        <input type="checkbox" class= "opacity-0" on:change={changeFunc(X.name,X.check)} bind:checked={X.check}>
-                        <p class="float-right overflow-hidden">{X.name}</p>
+                        <input type="checkbox" class= "hidden" on:change={changeFunc(X.name,X.check)} bind:checked={X.check}>
+                        <p class="flex-grow">{X.name}</p>
                     </label>
                 {:else}
                     <label 
                         class='box flex flex-row py-2 px-2 hover:text-primary-7 hover:bg-gray-100 cursor-pointer align-middle 
                         text-neutral-5 font-bold border-2 items-center'
-                        style="--theme-color: {X.color}; border-color: var(--theme-color);"
+                        style={`border-color: ${X.color}`}
                     >
                         <p class="overflow-hidden">{X.name}</p>
                         <input type="checkbox" class= "opacity-0" on:change={changeFunc(X.name,X.check)} bind:checked={X.check}>
